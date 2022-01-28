621b6f3657bcb72583230edc9edc8c4b0110638e
KalpanaShetty <kalpanashetty512@gmail.com>
2022-01-28 13:52:18 +0530

    Testcase2 automated

--- src/Sample43.java
+++ src/Sample43.java
@@ -2,5 +2,6 @@
 public class Sample43 {
 public static void main(String[] args) {
 	System.out.println("HI");
+	System.out.println("hi3");
 }
 }
---
+++ src/Sample52.java
@@ -0,0 +1,6 @@
+
+public class Sample52 {
+public static void main(String[] args) {
+	System.out.println("2nd time");
+}
+}
